3eb3e26ea0486ff8cb434870270894f0794de80d
Damian <novaplanetbiznes@gmail.com>
2025-11-05 14:26:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Tile from "./Tile";
 
 function App() {
+  const [tiles, setTiles] = useState([]);
+
+  useEffect(() => {
+    fetch("https://twoj-backend.vercel.app/api/tiles")
+      .then(res => res.json())
+      .then(data => setTiles(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      {tiles.map((t, i) => <Tile key={i} {...t} />)}
     </div>
   );
 }
